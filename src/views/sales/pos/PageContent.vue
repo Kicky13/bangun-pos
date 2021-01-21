7d13bfeefee25484cd6bea07336a49969c22e8b9
syifaq57 <kicky120@gmail.com>
2021-01-21 08:44:31 +0700

    POS Page

---
+++ src/views/sales/pos/PageContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-row class="match-height">
+    <b-col lg="6">
+      <item-section />
+    </b-col>
+    <b-col lg="6">
+      <cart-section />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { BRow, BCol } from 'bootstrap-vue'
+
+import CartSection from './CartSection.vue'
+import ItemSection from './ItemSection.vue'
+
+export default {
+  components: {
+    BRow,
+    BCol,
+
+    CartSection,
+    ItemSection,
+  },
+}
+</script>
---
+++ src/views/sales/pos/Pos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <page-content />
+  </div>
+</template>
+
+<script>
+import {} from 'bootstrap-vue'
+import PageContent from './PageContent.vue'
+
+export default {
+  components: {
+    PageContent,
+  },
+  data() {
+    return {
+      menuHidden: this.$store.state.appConfig.layout.menu.hidden,
+    }
+  },
+  created() {
+    this.$store.commit('appConfig/UPDATE_NAV_MENU_HIDDEN', true)
+  },
+  destroyed() {
+    this.$store.commit('appConfig/UPDATE_NAV_MENU_HIDDEN', this.menuHidden)
+  },
+}
+</script>
