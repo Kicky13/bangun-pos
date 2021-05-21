bb13e58f89418dfa6c151bbc117c9a3145fb223d
Fauzan Adhim <oadhim@gmail.com>
2021-05-21 15:36:15 +0700

    ganti variabel jum customer

--- src/views/master/toko/table/ListTable.vue
+++ src/views/master/toko/table/ListTable.vue
@@ -524,7 +524,7 @@ export default {
         shopNumber: data.telp_toko,
         shopLogo: null,
         jumTrans: data.qty_trans,
-        jumCust: data.tot_product,
+        jumCust: data.tot_cust,
         jumProduk: data.tot_product,
       }
       this.rows.push(res)
